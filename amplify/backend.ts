16994a245c07e9d6cc46be26b4a51e04e9a45dc8
BandFlow25 <jason@flowency.co.uk>
2026-02-04 12:03:35 +0000

    fix: add RemovalPolicy.RETAIN to prevent DynamoDB data loss

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,5 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { RemovalPolicy } from 'aws-cdk-lib';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
@@ -14,5 +15,10 @@ const backend = defineBackend({
   auth,
   data,
 });
+
+// Retain the DynamoDB table on stack deletion to prevent data loss
+backend.data.resources.cfnResources.amplifyDynamoDbTables['Node'].applyRemovalPolicy(
+  RemovalPolicy.RETAIN
+);
 
 export default backend;
